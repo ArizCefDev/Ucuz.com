4eaeceed95fe9dce0f7fd6844510de0fb051d7b2
ArizCefDev <arizcefdev@gmail.com>
2023-07-10 19:38:20 +0400

    update project 10/23

---
+++ Ucuz.com/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ucuz.com.Controllers
+{
+	public class ContactController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Ucuz.com/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ucuz.com.Controllers
+{
+	public class LoginController : Controller
+	{
+		public IActionResult SignIn()
+		{
+			return View();
+		}
+
+		public IActionResult SignUp()
+		{
+			return View();	
+		}
+	}
+}
